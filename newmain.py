d43136b6a74fbae03d67ce985f640cf974661f35
PatricJunior <patrickouam11@gmail.com>
2026-03-13 08:42:59 +0100

    Ich habe Quadrat hinzugefügt

--- newmain.py
+++ newmain.py
@@ -2,3 +2,8 @@ def hallo (name):
     return f"Hallo {name} schön, dass du da bist !!!"
 
 print(hallo("ANGELLA"))
+
+def quadrat(zahl):
+    return f" Das Quadrat von {zahl} ist {zahl*zahl} "
+
+print (quadrat (50))
